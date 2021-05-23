6a6af9904cd61b541a1c6e9817fad58625339799
akashdecoder <akash.ranjan1999@gmail.com>
2021-05-24 01:31:43 +0530

    update

--- src/main/java/com/springboot/myprofile/PageController.java
+++ src/main/java/com/springboot/myprofile/PageController.java
@@ -1,14 +1,27 @@
 package com.springboot.myprofile;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Controller
 public class PageController {
@@ -19,7 +32,27 @@ public class PageController {
     Logger logger = LoggerFactory.getLogger(PageController.class);
 
     @GetMapping("/")
-    public String showHomePage() {
+    public String showHomePage(Model model) throws IOException {
+        List<Tags> linkslists = new ArrayList<Tags>();
+        String arr[] = new String[3];
+        FileInputStream fin = new FileInputStream(new File("src/main/links.xlsx"));
+        XSSFWorkbook wb = new XSSFWorkbook(fin);
+        XSSFSheet sheet = wb.getSheetAt(0);
+        Iterator<Row> ritr = sheet.iterator();
+        ritr.next();
+        int index=0;
+        while(ritr.hasNext()) {
+            index = 0;
+            Row row = ritr.next();
+            Iterator<Cell> citr = row.cellIterator();
+            while(citr.hasNext()) {
+                Cell cell = citr.next();
+                arr[index++] = cell.getStringCellValue();
+            }
+            linkslists.add(new Tags(arr[0], arr[1], arr[2]));
+        }
+        model.addAttribute("linkslists", linkslists);
+
         return "profilepage";
     }
     
