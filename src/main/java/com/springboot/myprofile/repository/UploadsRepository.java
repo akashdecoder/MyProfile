dd2167b0ee09f1fca8854a2e3f72605b0bc52557
akashdecoder <akash.ranjan1999@gmail.com>
2021-06-23 23:21:33 +0530

    Updates

--- src/main/java/com/springboot/myprofile/repository/UploadsRepository.java
+++ src/main/java/com/springboot/myprofile/repository/UploadsRepository.java
@@ -2,10 +2,11 @@ package com.springboot.myprofile.repository;
 
 import com.springboot.myprofile.model.Uploads;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-
+@Repository
 public interface UploadsRepository extends JpaRepository<Uploads, Long> {
     List<Uploads> findAll();
 }
